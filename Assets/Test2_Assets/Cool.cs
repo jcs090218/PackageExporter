cd31df76acb181f4dfedaf6fee009549b8c98a74
jenchieh <jcs090218@gmail.com>
2017-10-24 15:53:41 +0800

    test projects, just for testing purposes.

---
+++ Assets/Test2_Assets/Cool.cs
@@ -0,0 +1,21 @@
+﻿/**
+ * $File: Cool.cs $
+ * $Date: #CREATIONDATE# $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information 
+ *	                 Copyright (c) #CREATEYEAR# by Shen, Jen-Chieh $
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// 
+/// </summary>
+public class Cool 
+    : MonoBehaviour 
+{
+    // Empty test file.    
+}
